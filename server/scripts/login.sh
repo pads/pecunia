8d777452c6f4e7fa441a082ce88c3c8080a5bd68
Ben Paddock <pads@thisispads.me.uk>
2017-12-02 16:53:27 +0000

    Add script to login

---
+++ server/scripts/login.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+source .env
+
+if [ $# -eq 0 ]
+  then
+    echo "Usage: login.sh <state>"
+    exit -1
+fi
+
+if [[ -z "${CLIENT_ID}" ]]; then
+  echo "Set CLIENT_ID"
+  exit -1
+fi
+
+STATE=$1
+
+open "https://auth.getmondo.co.uk/?client_id=$CLIENT_ID&redirect_uri=http://localhost:3000/oauth/callback&response_type=code&state=$STATE"
