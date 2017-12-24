c85294a4b6fc4e6d9da1f7ad8e316324c8e128f2
Ben Paddock <pads@thisispads.me.uk>
2017-12-24 15:47:51 +0000

    Add missing imports and remove redundant tests

--- client/src/app/monzo/monzo.effect.spec.ts
+++ client/src/app/monzo/monzo.effect.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { MonzoEffect } from './monzo.effect';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { Actions } from '@ngrx/effects';
+import { ScannedActionsSubject } from '@ngrx/store';
 
 describe('MonzoEffect', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MonzoEffect]
+      providers: [MonzoEffect, HttpClient, HttpHandler, Actions, ScannedActionsSubject]
     });
   });
 
